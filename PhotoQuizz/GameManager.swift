5a11132b3ec1249bb40d076304980ffe5d272f87
Bastien MICHA <bastien.micha@inter-invest.fr>
2025-11-30 19:12:32 +0100

    Delete PhotoQuizz directory

--- PhotoQuizz/GameManager.swift
+++
@@ -1,7 +0,0 @@
-//
-//  GameManager.swift
-//  PhotoQuizz
-//
-//  Created by Bastien Micha on 01/05/2025.
-//
-
--- PhotoQuizz/RevealViewController.swift
+++
@@ -1,7 +0,0 @@
-//
-//  RevealViewController.swift
-//  PhotoQuizz
-//
-//  Created by Bastien Micha on 01/05/2025.
-//
-
